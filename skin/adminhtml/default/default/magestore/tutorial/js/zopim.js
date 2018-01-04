e8017e3d6a275e93e4385d1fa35e9f320202f968
luna <hoangthamhtth@gmail.com>
2018-01-04 08:58:06 +0700

    Show zopim chat like Magestore in backend

---
+++ skin/adminhtml/default/default/magestore/tutorial/js/zopim.js
@@ -0,0 +1,18 @@
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-24736539-2', 'auto');
+ga('require', 'GTM-KCK3CGG')
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
+ga('require', 'ec');
+ga('send', 'pageview');
+
+
+window.onload = function() {
+  window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set._.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');$.src='//v2.zopim.com/?7i311xtUlkp695O3yEAWomWAyjHgRZ9X';z.t=+new Date;$.type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');//ga('create', 'UA-58372331-1', 'auto', {'name': 'nto'});ga('nto.send', 'pageview');
+}
